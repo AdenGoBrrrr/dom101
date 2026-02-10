02d906553afb2bbb06b04b9989c422906fdc4ed6
AdenGoBrrrr <adenalhamis2006@gmail.com>
2026-02-10 05:24:45 +0000

    progress

--- index.js
+++ index.js
@@ -11,3 +11,58 @@
 'use strict';
 
 // add your functions here
+
+function replaceText(elem, str){
+    elem.textContent = str;
+}
+
+function addTextTo(elem, str){
+    elem.textContent += str;
+}
+
+function moreBears(){
+    animals.src = 'http://placebear.com/400/200';
+    animals.alt = 'A bear.';
+    animals.title = 'A BEAR!';
+}
+
+function setId(elem, str){
+    elem.id = str;
+    return elem;
+}
+
+function setClass(elem, str){
+    elem.className = str;
+    return elem;
+}
+
+function addAClass(elem, str){
+    elem.classList.add(str);
+    return elem;    
+}
+
+function removeAClass(elem, str){
+    elem.classList.remove(str);
+    return elem;
+}
+
+function newElement(name){
+    return document.createElement(name);
+}
+
+function findElementById(id){
+    return document.getElementById(id);
+}
+
+function findElementsByQuery(query){
+    return document.querySelectorAll(query);
+}
+
+function reverseList(query){
+    const startlist = document.querySelector(query);
+    let reversedlist = [];
+    for (let i = startlist.length - 1; i >= 0; i--){
+        reversedlist.push(startlist[i]);
+    }
+    return reversedlist;
+}
